730b86d9e9f461dd298219db721b620c5037497b
Elimax Mwangi <elimax@localhost.localdomain>
2019-11-18 14:18:56 -0500

    routing through push method

--- src/components/AddMeetup.js
+++ src/components/AddMeetup.js
@@ -3,10 +3,48 @@ import axios from 'axios';
 import {link} from 'react-router-dom';
 
 export default class AddMeetup extends Component {
+
+addMeetup(newMeetup){
+    console.log(newMeetup);
+   axios.request({
+       method: 'post',
+       url: 'http://localhost:3000/api/meetupzs',
+       data: newMeetup
+   }).then(response =>{
+        this.props.history.push("/");
+   }).catch(err =>console.log(err));
+}
+
+    onSubmit(e){
+       const newMeetup ={
+           name: this.refs.name.value,
+           city: this.refs.city.value,
+           address: this.refs.address.value
+       }
+       this.addMeetup(newMeetup);
+        e.preventDefault();
+    }
+   
     render() {
         return (
             <div>
+                <br />
                 <h1>Add AddMeetup</h1>
+                <form onSubmit={this.onSubmit.bind(this)}>
+                    <div className="input-field" >
+                        <input type="text" name="name"ref="name"/>
+                        <label  htmlFor="name">name</label>
+                    </div>
+                    <div className="input-field" >
+                        <input type="text" name="city"ref="city"/>
+                        <label  htmlFor="city">city</label>
+                    </div>
+                    <div className="input-field" >
+                        <input type="text" name="address"ref="address"/>
+                        <label  htmlFor="address">address</label >
+                    </div>
+                    <input type="submit" className="btn" value="save" />
+                </form>
             </div>
         )
     }
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,27 +5,29 @@ import About from './About';
 import MeetupDetail from './MeetupDetail';
 import AddMeetup from './AddMeetup';
 
+
+
 const Main = () => {
     return (
         <Switch>
-  <Route exact path="/" Component={Meetsup} >
-      <Meetsup/>
-  </Route>
-  <Route exact path="/about" Component={About}>
-      <About />
-  </Route>
+            <Route exact path="/" Component={Meetsup} >
+                <Meetsup/>
+            </Route>
+            <Route exact path="/about" Component={About}>
+                <About />
+            </Route>
   {/* <Route exact path="/meetsup/:id" Component={MeetupDetail}>
       <MeetupDetail />
   </Route> */}
 
-        <Route exact path="/meetsup/:id" 
-            render={(props) => <MeetupDetail {...props} />}
-        />
-        <MeetupDetail />
-        
-        <Route exact path="/addMeetup" Component={AddMeetup}>
-            <AddMeetup/>
-        </Route>
+            <Route exact path="/meetsup/:id" render={({history})  => <AddMeetup {...history} history={history} />} />
+                <AddMeetup/>
+
+            <Route exact path="/meetsup/:id" 
+                render={(props) => <MeetupDetail {...props} />}
+            />
+            <MeetupDetail />
+
         </Switch>
     );
 }
